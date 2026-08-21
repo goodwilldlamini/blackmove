88c1a546f79af01107056846bf0c38bcc4e31eb3
Goodwill Dlamini <goodwill@eddicus.dev>
2026-08-21 10:38:36 +0200

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
-    nitro(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
